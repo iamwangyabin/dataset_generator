9fd66014c666b948192db8ce7a88858daa8254d3
Wang Yabin <38123329+iamwangyabin@users.noreply.github.com>
2024-08-31 09:29:05 +0100

    Update generator_sd3.py

--- fakepop/generator_sd3.py
+++ fakepop/generator_sd3.py
@@ -36,8 +36,43 @@ combined_dict.update(file3)
 output_dir = 'stable-diffusion-3-medium'
 os.makedirs(output_dir, exist_ok=True)
 
-def make_divisible_by_8(value):
-    return (value // 16) * 16
+IMAGE_FACTOR = 28
+MIN_PIXELS = 4 * 28 * 28
+MAX_PIXELS = 16384 * 28 * 28
+MAX_RATIO = 200
+
+def round_by_factor(number: int, factor: int) -> int:
+    """Returns the closest integer to 'number' that is divisible by 'factor'."""
+    return round(number / factor) * factor
+
+
+def ceil_by_factor(number: int, factor: int) -> int:
+    """Returns the smallest integer greater than or equal to 'number' that is divisible by 'factor'."""
+    return math.ceil(number / factor) * factor
+
+
+def floor_by_factor(number: int, factor: int) -> int:
+    """Returns the largest integer less than or equal to 'number' that is divisible by 'factor'."""
+    return math.floor(number / factor) * factor
+
+def smart_resize(
+    height: int, width: int, factor: int = IMAGE_FACTOR, min_pixels: int = MIN_PIXELS, max_pixels: int = MAX_PIXELS
+) -> tuple[int, int]:
+    if max(height, width) / min(height, width) > MAX_RATIO:
+        raise ValueError(
+            f"absolute aspect ratio must be smaller than {MAX_RATIO}, got {max(height, width) / min(height, width)}"
+        )
+    h_bar = max(factor, round_by_factor(height, factor))
+    w_bar = max(factor, round_by_factor(width, factor))
+    if h_bar * w_bar > max_pixels:
+        beta = math.sqrt((height * width) / max_pixels)
+        h_bar = floor_by_factor(height / beta, factor)
+        w_bar = floor_by_factor(width / beta, factor)
+    elif h_bar * w_bar < min_pixels:
+        beta = math.sqrt(min_pixels / (height * width))
+        h_bar = ceil_by_factor(height * beta, factor)
+        w_bar = ceil_by_factor(width * beta, factor)
+    return h_bar, w_bar
 
 
 for file_path in tqdm(list(combined_dict.keys())):
@@ -51,8 +86,11 @@ for file_path in tqdm(list(combined_dict.keys())):
         total_pixels = ran_size*ran_size
         width = int(np.sqrt(total_pixels * aspect_ratio))
         height = int(total_pixels / width)
+
+        h_bar, w_bar = smart_resize(height, width, 64)
+
         image = pipe(prompt=promp,
-                    height=make_divisible_by_8(height), width=make_divisible_by_8(width),
+                    height=h_bar, width=w_bar,
                     guidance_scale=7.0, num_images_per_prompt=1, num_inference_steps=28,
                     ).images[0]
         image.save(output_file)
