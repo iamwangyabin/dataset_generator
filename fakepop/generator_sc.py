3f3d03be0ce4a136b38396942cf99570a525cbab
Wang Yabin <38123329+iamwangyabin@users.noreply.github.com>
2024-09-03 09:52:12 +0100

    Update generator_sc.py

--- fakepop/generator_sc.py
+++ fakepop/generator_sc.py
@@ -46,7 +46,7 @@ output_dir = 'stable-cascade'
 os.makedirs(output_dir, exist_ok=True)
 
 def make_divisible_by_8(value):
-    return (value // 2) * 2
+    return (value // 24) * 24
 
 file_paths = list(combined_dict.keys())[:3000]
 random.shuffle(file_paths)
